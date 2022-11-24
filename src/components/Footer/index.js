83dc247a02670450f8aceb69cc32b280abede567
Carlos Pessoa <carlos.henriquepessoa15@gmail.com>
2022-11-24 03:31:50 -0300

    criado o componente footer da aplicação

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,30 @@
 import './style.css';
+import Logo from '../../assets/logo.svg'
+import FacebookLogo from '../../assets/facebook.svg';
+import IntagramLogo from '../../assets/instagram.svg';
 
 function Footer() {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer>
+            <div className='left-footer'>
+                <div className='container-address'>
+                    <strong>Endereço:</strong>
+                    <span>Rua Cubos, 10</span>
+                    <span>Jardim Academy</span>
+                    <span>Salvador - Bahia - CEP 41820-021</span>
+                </div>
+                <div className='container-social'>
+                    <img src={FacebookLogo} alt='facebook logo' />
+                    <img src={IntagramLogo} alt='instagram logo' />
+                </div>
+            </div>
+
+            <div className='vertical-line'></div>
+
+            <div className='right-footer'>
+                <img src={Logo} alt='Logo footer' />
+            </div>
+        </footer>
     )
 }
 
